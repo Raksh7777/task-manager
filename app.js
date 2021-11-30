cc1febe32237f79ce4f88d3fba5b15e89e779fb0
Rakshika Bawari <rakshika7777@gmail.com>
2021-12-01 01:05:04 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+app.use(express.static("./public"));
+app.use(express.json());
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server listening at ${port}!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
